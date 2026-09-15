965e300b5c3b0cb90d76d21f7d50827c2f5c2aa2
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-09-15 23:37:07 +0100

    wops-19 implement bronze weather data extraction

--- extraction/extract_cities.py
+++ extraction/extract_cities.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 import requests
 
-output = Path("data/bronze/cities.csv")
+
+stored_cities = Path("data/bronze/cities.csv")
 url = "https://simplemaps.com/static/data/country-cities/ma/ma.csv"
+
 try:
     response = requests.get(url, timeout=30)
     response.raise_for_status()
 
-    with open(output, "wb") as f:
+    with open(stored_cities, "wb") as f:
         f.write(response.content)
     print("cities downloaded successfully")
 except requests.exceptions.RequestException as e:
---
+++ extraction/extract_weather.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import requests
+import pandas as pd
+import json
+
+stored_cities = Path("data/bronze/cities.csv")
+stored_weather = Path("data/bronze/weather.json")
+weather_url = "https://api.open-meteo.com/v1/forecast"
+
+
+cities = pd.read_csv(stored_cities)
+
+latitudes = ",".join(cities["lat"].astype(str))
+longitudes = ",".join(cities["lng"].astype(str))
+
+
+params = {
+    "latitude": latitudes,
+    "longitude": longitudes,
+    "daily": [
+        "temperature_2m_max",
+        "temperature_2m_min",
+        "precipitation_sum",
+        "precipitation_probability_max",
+        "wind_speed_10m_max",
+        "wind_gusts_10m_max",
+        "weather_code",
+    ],
+    "timezone": "auto",
+}
+
+response = requests.get(weather_url, params=params, timeout=30)
+response.raise_for_status()
+
+weather_data = response.json()
+
+with open(stored_weather, "w", encoding="utf-8") as f:
+    json.dump(weather_data, f, ensure_ascii=False, indent=4)
