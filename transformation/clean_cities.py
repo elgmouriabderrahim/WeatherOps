42ec145202c6a4b4ec03ba53b133cf9929e49eb6
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-09-16 15:11:39 +0100

    wops-26 standarize columns names #done

--- transformation/clean_cities.py
+++ transformation/clean_cities.py
@@ -6,4 +6,17 @@ cities = pd.read_csv("data/bronze/cities.csv")
 with open("data/bronze/weather.json", "r", encoding="utf-8") as f:
     weather = json.load(f)
 
+cities = cities.rename(columns={"city": "city_name", "lat": "latitude", "lng": "longitude"})
 
+weather_df = []
+for forecast in weather:
+    df = pd.DataFrame(forecast["daily"])
+    df["latitude"] = forecast["latitude"]
+    df["longitude"] = forecast["longitude"]
+
+    weather_df.append(df)
+
+weather_df = pd.concat(weather_df, ignore_index=True)
+weather_df = weather_df.rename(columns={"time": "date", "temperature_2m_max": "temperature_max", "temperature_2m_min": "temperature_min", "precipitation_sum": "precipitation", "precipitation_probability_max": "precipitation_probability", "wind_speed_10m_max": "wind_speed", "wind_gusts_10m_max": "wind_gust"})
+
+print(weather_df)
