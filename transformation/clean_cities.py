1ed76966500aa2b46dc61985b2a694da3089b4ab
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-09-16 16:02:29 +0100

    wops-28 srop missing values #done

--- transformation/clean_cities.py
+++ transformation/clean_cities.py
@@ -2,21 +2,30 @@ import pandas as pd
 import json
 
 cities = pd.read_csv("data/bronze/cities.csv")
-
 with open("data/bronze/weather.json", "r", encoding="utf-8") as f:
     weather = json.load(f)
 
-cities = cities.rename(columns={"city": "city_name", "lat": "latitude", "lng": "longitude"})
 
+cities = cities.rename(columns={"city": "city_name", "lat": "latitude", "lng": "longitude"})
 weather_df = []
 for forecast in weather:
     df = pd.DataFrame(forecast["daily"])
     df["latitude"] = forecast["latitude"]
     df["longitude"] = forecast["longitude"]
-
     weather_df.append(df)
 
-weather_df = pd.concat(weather_df, ignore_index=True)
-weather_df = weather_df.rename(columns={"time": "date", "temperature_2m_max": "temperature_max", "temperature_2m_min": "temperature_min", "precipitation_sum": "precipitation", "precipitation_probability_max": "precipitation_probability", "wind_speed_10m_max": "wind_speed", "wind_gusts_10m_max": "wind_gust"})
+weather_df = pd.concat(weather_df, ignore_index=True).dropna()
+weather_df = weather_df.rename(columns={
+    "time": "date",
+    "temperature_2m_max": "temperature_max_c",
+    "temperature_2m_min": "temperature_min_c",
+    "precipitation_sum": "precipitation_mm",
+    "precipitation_probability_max": "precipitation_probability_pct",
+    "wind_speed_10m_max": "wind_speed_kmh",
+    "wind_gusts_10m_max": "wind_gust_kmh",
+    "weather_code": "weather_code"
+})
 
+cities = cities[["city_name", "latitude", "longitude"]].dropna()
 print(weather_df)
+print(cities)
