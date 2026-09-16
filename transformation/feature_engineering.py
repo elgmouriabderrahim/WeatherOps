cb812d8e24f3116d19bb318659b4264f0e82f8d2
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-09-17 00:14:32 +0100

    wops-33 create temperature categorie #done

---
+++ transformation/feature_engineering.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+weather_df = pd.read_csv("data/silver/clean_weather_data.csv")
+
+weather_df["temperature_category"] = pd.cut(
+    weather_df["temperature_max_c"],
+    bins=[-float("inf"), 10, 25, 35, float("inf")],
+    labels=["cold", "normal", "hot", "extreme"]
+)
