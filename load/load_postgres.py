7eab47e21715b65817baaf694d3b0fcac092bf9e
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-09-17 19:54:05 +0100

    wops-44 add weather data loading into db #done

--- load/load_postgres.py
+++ load/load_postgres.py
@@ -23,3 +23,35 @@ print("Database connection successful")
 
 cities = pd.read_csv('data/silver/clean_cities.csv')
 cities.to_sql("cities", engine, if_exists="append", index=False)
+
+weather = pd.read_csv("data/gold/featured_data.csv")
+db_cities = pd.read_sql("SELECT id, city_name, latitude, longitude FROM cities", engine)
+weather = weather.merge(db_cities, on=["city_name", "latitude", "longitude"], how="left")
+
+weather = weather.rename(columns={
+    "id": "city_id",
+    "date": "forecast_date"
+})
+
+weather = weather[[
+    "city_id",
+    "forecast_date",
+    "temperature_max_c",
+    "temperature_min_c",
+    "precipitation_mm",
+    "precipitation_probability_pct",
+    "wind_speed_kmh",
+    "wind_gust_kmh",
+    "weather_code",
+    "risk_score",
+    "risk_level",
+    "temperature_category",
+    "precipitation_category",
+    "wind_category",
+    "temperature_risk",
+    "wind_risk",
+    "precipitation_risk",
+    "weather_code_risk",
+]]
+
+weather.to_sql("weather_forecasts", engine, if_exists="append", index=False)
