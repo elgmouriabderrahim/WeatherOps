4d7d281af69414722a8a533ec3062be5a45f5b16
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-09-18 12:17:19 +0100

    feat(wops-55): add city filter #done

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from getdata import df
+import pandas as pd
 
 st.title("WeatherOps Dashboard")
 st.write("displaying weather data and analysis.")
@@ -21,3 +22,14 @@ col3.metric("Min Temperature", f"{min_temperature} °C")
 col4.metric("Max Precipitation", f"{max_precipitation} mm")
 col5.metric("Risky Periods", risky_periods)
 col6.metric("Highest Risk City", highest_risk_city)
+
+
+
+select_city = st.selectbox("Select a city to view its weather data:",["All"] + sorted(df["city_name"].unique()))
+
+if select_city == "All":
+    filtered_df = df
+else:
+    filtered_df = df[df["city_name"] == select_city]
+
+st.dataframe(filtered_df)
